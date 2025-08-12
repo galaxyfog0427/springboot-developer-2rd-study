13b33dd3a8853ae507ff8832433e3ffb8a030ecf
galaxyfog <galaxyfog@naver.com>
2025-08-12 15:49:17 +0900

    Add UserNotFoundException in UserService

---
+++ src/test/java/me/parkjeounghyun/springbootdeveloper/service/UserServiceTest.java
@@ -0,0 +1,29 @@
+package me.parkjeounghyun.springbootdeveloper.service;
+
+import lombok.RequiredArgsConstructor;
+import me.parkjeounghyun.springbootdeveloper.config.error.exception.UserNotFoundException;
+import me.parkjeounghyun.springbootdeveloper.domain.User;
+import me.parkjeounghyun.springbootdeveloper.repository.UserRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class UserServiceTest {
+
+    @Autowired
+    UserService userService;
+
+    @DisplayName("findById: 없는 유저의 아이디를 찾으면 UserNotFoundException이 발생한다.")
+    @Test
+    public void userNotFound() {
+        // then
+        Assertions.assertThrows(UserNotFoundException.class, () -> {
+            userService.findById(1L);
+        });
+    }
+}
